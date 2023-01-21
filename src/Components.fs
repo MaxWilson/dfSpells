75741b1d359dd5787432fc27506e15d1cc5df0da
Max Wilson <wilson.max@gmail.com>
2023-01-21 06:43:04 -0800

    Allow toggling

--- src/Components.fs
+++ src/Components.fs
@@ -15,27 +15,34 @@ let Counter() =
 
 open Spells
 
+module Tup2 =
+    let create x y = x,y
+module List =
+    let number lst = lst |> List.mapi (fun ix v -> v, ix)
 
-type SpellData = {
-    data: Spell
-    chains: PotentialChain list
-    }
-and PotentialChain = {
-    heading: MagicType
-    items: PrereqData list list
-}
-and PrereqData = Expanded of Spell | Unexpanded of string
+[<AutoOpen>]
+module Html =
+    let class' (className: string) ctor (children: _ list) = ctor [prop.children children; prop.className className]
+    let classTxt (className: string) ctor (txt: string) = ctor [prop.text txt; prop.className className]
 
 [<ReactComponent>]
-let Spell (spell:SpellData) =
+let Spell (spell:Spell) =
+    let choice, update = React.useState(None)
     Html.div [
-        Html.h3 spell.data.name
-        for p in spell.chains do
-            let class' (className: string) ctor (children: _ list) = ctor [prop.children children; prop.className className]
-            let classTxt (className: string) ctor (txt: string) = ctor [prop.text txt; prop.className className]
-            for chain in p.items do
+        Html.h3 spell.name
+        for p, pix in spell.prereqs |> List.number do
+            for chain, cix in p.items |> List.number do
                 let prereqs = System.String.Join(" and ", chain)
-                classTxt "prereq" Html.div $"{p.heading}: {prereqs}"
+                match choice with
+                | Some choice when choice <> (pix, cix) -> ()
+                | _ ->
+                    let toggle _ =
+                        if choice.IsNone then update (Some(pix, cix))
+                        else update None
+                    class' "prereqs" Html.div [
+                        Html.input [prop.type'.checkbox; prop.isChecked choice.IsSome; prop.onClick(toggle)]
+                        Html.label [prop.text $"{p.heading}: {prereqs} {(pix, cix)}"; prop.className "label"; prop.onClick(toggle)]
+                        ]
         ]
 
 [<ReactComponent>]
